91b05d29790fb2e6d547b87ae54246723c03c560
ericweidman <ericweidman@gmail.com>
2017-03-14 13:48:23 -0400

    Stubbed routes

--- src/main/java/com/controllers/CyberCareController.java
+++ src/main/java/com/controllers/CyberCareController.java
@@ -3,6 +3,9 @@ package com.controllers;
 import com.services.CustomerRepository;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -30,6 +33,31 @@ public class CyberCareController {
     public void destroy() {
         dbui.stop();
     }
+
+    @RequestMapping(path = "/create-customer", method = RequestMethod.POST)
+    public void addCustomer(@RequestBody String json){
+
+        System.out.println(json);
+    }
+
+    @RequestMapping(path = "/get-customers", method = RequestMethod.GET)
+    public String customerList(){
+
+        return "customers";
+    }
+
+    @RequestMapping(path = "/update-customer/{id}", method = RequestMethod.PUT)
+    public void updateCustomer(@RequestBody String json){
+
+        System.out.println(json);
+    }
+
+    @RequestMapping(path = "/delete-customer/{id}", method = RequestMethod.DELETE)
+    public void deleteCustomer(@RequestBody int id){
+        customers.delete(id);
+    }
+
+
 
 }
 
